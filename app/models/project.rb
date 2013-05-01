4eec992c2d90b0a35b77c7ce5a2057ade8cdf886
Andreas Marc Klingler <post@andreas-klingler.de>
2013-05-01 09:39:13 +0200

    #41 Attribute directory to the project model added.

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,12 @@
 class Project < ActiveRecord::Base
-  attr_accessible :name, :default_language
+  attr_accessible :name, :default_language, :directory
 
   # ---------------------------------------------------------------------------------------
   validates                 :name, presence: true
   validates_uniqueness_of   :name, { :case_sensitive => false }
+
+  # ---------------------------------------------------------------------------------------
+  validates                 :directory, presence: true
 
   # ---------------------------------------------------------------------------------------
   validates                 :default_language, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130430205133) do
+ActiveRecord::Schema.define(:version => 20130501073313) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
     t.string   "default_language"
+    t.string   "directory"
   end
 
 end
